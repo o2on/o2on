6bded91fe383131c449a5f5cdfbe7e72ff07907b
k-uehara <k-uehara@ac9984d6-f82f-0410-8253-bf21b04e86e2>
2008-03-08 16:23:24 +0000

    トレイアイコンのチップに送受信速度を表示 デフォルトで全板をdat保存対象に設定する jkl-parsexml.jsのアップデート
    * 以下バグフィックス
    ブロードキャストメッセージのループ
    クォータが制限値を超えるとタスクトレイから操作できなくなる
    管理画面でのSQL実行結果が1件少ない
    プロキシ使用時のハンドルリーク
    HTML化した過去ログをプロキシで取得時にgzip圧縮でdatが保存される
    
    git-svn-id: http://svn.sourceforge.jp/svnroot/o2on@42 ac9984d6-f82f-0410-8253-bf21b04e86e2

---
+++ tags/build0042/o2on/src.o2on/O2Job_DatCollector.h
@@ -0,0 +1,141 @@
+/* Copyright (C) 2006 o2on project. All rights reserved.
+ * http://o2on.net/
+ */
+
+/*
+ * project		: o2on
+ * filename		: O2Job_DatCollector.h
+ * description	: 
+ *
+ */
+
+#pragma once
+#include "O2Job.h"
+#include "O2Logger.h"
+#include "O2Profile.h"
+#include "O2NodeDB.h"
+#include "O2KeyDB.h"
+#include "O2DatIO.h"
+#include "O2Boards.h"
+#include "O2Client.h"
+#include "O2Protocol_Dat.h"
+#include "dataconv.h"
+#include "../cryptopp/osrng.h"
+
+
+
+
+class O2Job_DatCollector
+	: public O2Job
+	, public O2Protocol_Dat
+{
+protected:
+	O2Logger	*Logger;
+	O2Profile	*Profile;
+	O2NodeDB	*NodeDB;
+	O2KeyDB		*KeyDB;
+	O2KeyDB		*QueryDB;
+	O2DatIO		*DatIO;
+	O2Boards	*Boards;
+	O2Client	*Client;
+	HWND		hwndBaloonCallback;
+	UINT		msgBaloonCallback;
+
+public:
+	O2Job_DatCollector(const wchar_t	*name
+					 , time_t			interval
+					 , bool				startup
+					 , O2Logger			*lgr
+					 , O2Profile		*prof
+					 , O2NodeDB			*ndb
+					 , O2KeyDB			*kdb
+					 , O2KeyDB			*qdb
+					 , O2DatIO			*dio
+					 , O2Boards			*boards
+					 , O2Client			*client)
+		: O2Job(name, interval, startup)
+		, Logger(lgr)
+		, Profile(prof)
+		, NodeDB(ndb)
+		, KeyDB(kdb)
+		, QueryDB(qdb)
+		, DatIO(dio)
+		, Boards(boards)
+		, Client(client)
+		, hwndBaloonCallback(NULL)
+		, msgBaloonCallback(0)
+	{
+	}
+
+	~O2Job_DatCollector()
+	{
+	}
+
+	void SetBaloonCallbackMsg(HWND hwnd, UINT msg)
+	{
+		hwndBaloonCallback = hwnd;
+		msgBaloonCallback = msg;
+	}
+
+	void JobThreadFunc(void)
+	{
+		// 取得する板を決める
+		wstrarray boards;
+		if (Boards->GetExList(boards) == 0)
+			return;
+		CryptoPP::AutoSeededRandomPool rng;
+		wstring &board = boards[rng.GenerateWord32(0, boards.size()-1)];
+
+		// ノード一覧を取得
+		O2NodeDB::NodeListT nodes;
+		if (Boards->GetExNodeList(board.c_str(), nodes) == 0)
+			return;
+
+		bool imported = false;
+
+		O2NodeDB::NodeListT::iterator it;
+		for (it = nodes.begin(); it != nodes.end() && !imported && IsActive(); it++) {
+			it->lastlink = 0;
+			it->reset();
+
+			// リクエスト発行
+			O2SocketSession ss;
+			ss.ip = it->ip;
+			ss.port = it->port;
+			MakeRequest_Dat(NULL, board.c_str(), &ss, Profile, DatIO, ss.sbuff);
+
+			Client->AddRequest(&ss);
+			ss.Wait();
+
+			HTTPHeader *header = (HTTPHeader*)ss.data;
+			if (CheckResponse(&ss, header, NodeDB, *it)) {
+				// 本文のdat取り込み
+				const char *rawdata = &ss.rbuff[header->length];
+				uint64 datsize = ss.rbuff.size() - header->length;
+				imported = ImportDat(DatIO, Boards, *header, rawdata, datsize,
+					Logger, ss.ip, ss.port, QueryDB, hwndBaloonCallback, msgBaloonCallback);
+				//1個成功したら終わり
+			}
+			else if (header && header->status != 200) {
+				Boards->RemoveExNode(board.c_str(), *it);
+			}
+
+			if (header) delete header;
+
+			Sleep(IsActive() ? DAT_COLLECTOR_INTERVAL_MS : 0);
+		}
+
+		O2NodeDB::NodeListT::iterator endit = it;
+		for (it = nodes.begin(); it != endit && IsActive(); it++) {
+			if (it->lastlink) {
+				// 成功したノードをtouch
+				NodeDB->touch(*it);
+			}
+			else {
+				// 失敗したノードをremove
+				NodeDB->remove(*it);
+				KeyDB->DeleteKeyByNodeID(it->id);
+			}
+		}
+	}
+};
---
+++ tags/build0042/o2on/src.o2on/O2LagQueryQueue.h
@@ -0,0 +1,166 @@
+/* Copyright (C) 2006 o2on project. All rights reserved.
+ * http://o2on.net/
+ */
+
+/*
+ * project		: o2on
+ * filename		: O2LagQueryQueue.h
+ * description	: 
+ *
+ */
+
+#pragma once
+#include "O2KeyDB.h"
+#include "O2Profile.h"
+#include "mutex.h"
+#include "dataconv.h"
+#include "thread.h"
+
+
+
+
+class O2LagQueryQueue
+	: public Mutex
+{
+protected:
+	typedef std::map<hashT,O2Key> O2KeyMap;
+	typedef std::map<hashT,O2Key>::iterator O2KeyMapIt;
+
+	bool		Active;
+	HANDLE		ThreadHandle;
+	O2KeyMap	queries;
+	O2Logger	*Logger;
+	O2Profile	*Profile;
+	O2KeyDB		*QueryDB;
+	HWND		hwndBaloonCallback;
+	UINT		msgBaloonCallback;
+
+public:
+	O2LagQueryQueue(O2Logger	*lgr
+				  , O2Profile	*prof
+				  , O2KeyDB		*qdb)
+		: Active(false)
+		, ThreadHandle(NULL)
+		, Logger(lgr)
+		, Profile(prof)
+		, QueryDB(qdb)
+		, hwndBaloonCallback(NULL)
+		, msgBaloonCallback(0)
+	{
+		start();
+	};
+	~O2LagQueryQueue() {
+		stop();
+	};
+	void SetBaloonCallbackMsg(HWND hwnd, UINT msg)
+	{
+		hwndBaloonCallback = hwnd;
+		msgBaloonCallback = msg;
+	}
+
+public:
+	bool add(const O2DatPath &datpath, const uint64 size)
+	{
+		O2Key querykey;
+		querykey.ip = 0;
+		querykey.port = 0;
+		querykey.size = size;
+		datpath.gethash(querykey.hash);
+		datpath.geturl(querykey.url);
+		datpath.gettitle(querykey.title);
+		querykey.enable = true;
+
+		Lock();
+		{
+			O2KeyMapIt it = queries.find(querykey.hash);
+			if (it == queries.end()) {
+				queries.insert(O2KeyMap::value_type(querykey.hash, querykey));
+			}
+			else {
+				it->second.date = time(NULL);
+			}
+		}
+		Unlock();
+
+		return true;
+	}
+
+	bool remove(const O2DatPath &datpath)
+	{
+		hashT hash;
+		datpath.gethash(hash);
+
+		Lock();
+		{
+			O2KeyMapIt it = queries.find(hash);
+			if (it != queries.end())
+				queries.erase(it);
+		}
+		Unlock();
+		return true;
+	}
+
+private:
+	void start(void)
+	{
+		if (!ThreadHandle) {
+			Active = true;
+			ThreadHandle = (HANDLE)_beginthreadex(NULL, 0, StaticThread, (void*)this, 0, NULL);
+		}
+	}
+
+	void stop(void)
+	{
+		if (ThreadHandle) {
+			Active = false;
+			WaitForSingleObject(ThreadHandle, INFINITE);
+			CloseHandle(ThreadHandle);
+			ThreadHandle = NULL;
+		}
+	}
+
+	static uint WINAPI StaticThread(void *data)
+	{
+		O2LagQueryQueue *me = (O2LagQueryQueue*)data;
+		me->Checker();
+
+		//_endthreadex(0);
+		return (0);
+	}
+
+	void Checker(void)
+	{
+		while (Active) {
+			Lock();
+			for (O2KeyMapIt it = queries.begin(); it != queries.end(); ) {
+				O2Key &querykey = it->second;
+
+				TRACE(querykey.title.c_str());
+				TRACE(L"\n");
+
+				if (time(NULL) - querykey.date < 5) {
+					it++;
+					continue;
+				}
+
+				//ƒLƒ…[“o˜^‚©‚ç5•bŒo‰ß‚µ‚Ä‚¢‚½‚ç“o˜^
+				querykey.date = 0;
+				if (QueryDB->AddKey(querykey) == 1) {
+					if (Logger) {
+						Logger->AddLog(O2LT_INFO, L"LagQueryQueue", 0, 0,
+							L"ŒŸõ“o˜^ %s", querykey.url.c_str());
+					}
+
+					if (hwndBaloonCallback && Profile->IsBaloon_Query()) {
+						SendMessage(hwndBaloonCallback, msgBaloonCallback,
+							(WPARAM)L"ŒŸõ“o˜^", (LPARAM)querykey.url.c_str());
+					}
+					QueryDB->Save(Profile->GetQueryFilePath());
+				}
+				it = queries.erase(it);
+			}
+			Unlock();
+			Sleep(2500);
+		}
+	}
+};
