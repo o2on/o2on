575359ff1da21e1bf51e0e8c508d4b59a69bd3ae
k-uehara <k-uehara@ac9984d6-f82f-0410-8253-bf21b04e86e2>
2008-08-09 06:52:20 +0000

    build 0045 リリース
    git-svn-id: http://svn.sourceforge.jp/svnroot/o2on@117 ac9984d6-f82f-0410-8253-bf21b04e86e2

--- branches/BRANCH_0043/o2on/src.o2on/O2Version.h
+++ branches/BRANCH_0043/o2on/src.o2on/O2Version.h
@@ -4,7 +4,7 @@
 #define APP_NAME				"o2on"
 #define APP_VER_MAJOR			0
 #define APP_VER_MINOR			2
-#define APP_BUILDNO				44
+#define APP_BUILDNO				45
 #define APP_VER_PREFIX			"alpha"
 #define APP_VER_FORMAT			"%s %1d.%02d %s (build %04d)"
 #define USERAGENT_FORMAT		"%s/%.1f (%s/%1d.%02d.%04d; %s)"
